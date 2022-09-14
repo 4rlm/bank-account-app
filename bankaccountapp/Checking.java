75a5bed0d07b2d4675684ee9d8a0df42d88192f4
4rlm <4rlm@protonmail.ch>
2022-09-15 06:11:14 +0900

    list shared account properties

--- bankaccountapp/Checking.java
+++ bankaccountapp/Checking.java
@@ -2,6 +2,8 @@ package bankaccountapp;
 
 public class Checking extends Account {
   // List properties specific to a checking account
+  int debitCardNumber;
+  int debitCardPIN;
 
   // Constructor to initialize checking account properties
   public Checking(String name) {
--- bankaccountapp/Savings.java
+++ bankaccountapp/Savings.java
@@ -2,6 +2,8 @@ package bankaccountapp;
 
 public class Savings extends Account {
   // List properties specific to Savings account
+  int safetyDepositBoxID;
+  int safetyDepositBoxKey;
 
   // Constructor to initlialize settings for Savings properties
   public Savings(String name) {
