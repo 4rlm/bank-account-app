8c6ad9aa0e8a18e6339dc7f21b3af73572a5aa72
4rlm <4rlm@protonmail.ch>
2022-09-15 06:50:49 +0900

    initialize new accounts with constructors and super constructor

--- bankaccountapp/BankAccountApp.java
+++ bankaccountapp/BankAccountApp.java
@@ -2,8 +2,8 @@ package bankaccountapp;
 
 public class BankAccountApp {
   public static void main(String[] args) {
-    Checking chkacc1 = new Checking("Tom Wilson");
-    Savings savacc1 = new Savings("Rich Lowe");
+    Checking chkacc1 = new Checking("Tom Wilson", "987654321", 1500);
+    Savings savacc1 = new Savings("Rich Lowe", "123456789", 2500);
 
     // Read CSV File then create new accounts based on that data
   }
